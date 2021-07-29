51e1dfc7da82ee0135c152f6a328cc5861a20085
CorentinGaillard <unconfigured@null.spigotmc.org>
2021-07-29 22:52:35 +0200

    Adding ColorScale, not tested

---
+++ GDMC Village/src/com/amberstonedream/play/ColorScale.java
@@ -0,0 +1,61 @@
+package com.amberstonedream.play;
+
+import org.bukkit.Material;
+
+public class ColorScale {
+	private static final Material[] scale = new Material[] { Material.BLACK_CONCRETE, Material.OBSIDIAN,
+			Material.BLACK_WOOL,
+//			Material.BLACK_CONCRETE_POWDER,
+			Material.BLACKSTONE, Material.CRACKED_POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS,
+			Material.CHISELED_POLISHED_BLACKSTONE, Material.POLISHED_BLACKSTONE, Material.GRAY_CONCRETE,
+			Material.GRAY_WOOL, Material.NETHERITE_BLOCK,
+//			Material.GRAY_CONCRETE_POWDER,
+			Material.BLAST_FURNACE, Material.BASALT, Material.CYAN_TERRACOTTA, Material.BEDROCK,
+			Material.GRAY_GLAZED_TERRACOTTA, Material.ACACIA_WOOD,
+//			Material.POLISHED_BASALT,
+			Material.SMOKER, Material.FURNACE, Material.COBBLESTONE, Material.CRACKED_STONE_BRICKS,
+			Material.CHISELED_STONE_BRICKS, Material.STONE_BRICKS,
+//			Material.GRAVEL,
+//			Material.LIGHT_GRAY_CONCRETE,
+			Material.STONE, Material.POLISHED_ANDESITE, Material.ANDESITE, Material.DEAD_BRAIN_CORAL_BLOCK,
+			Material.DEAD_TUBE_CORAL_BLOCK, Material.DEAD_BUBBLE_CORAL_BLOCK, Material.DEAD_FIRE_CORAL_BLOCK,
+			Material.DEAD_HORN_CORAL_BLOCK, Material.LIGHT_GRAY_WOOL,
+//			Material.LIGHT_GRAY_CONCRETE_POWDER,
+			Material.LODESTONE, Material.SMOOTH_STONE, Material.CLAY, Material.DIORITE, Material.POLISHED_DIORITE,
+			Material.WHITE_CONCRETE, Material.IRON_BLOCK,
+//			Material.WHITE_CONCRETE_POWDER,
+			Material.BIRCH_WOOD, Material.BONE_BLOCK, Material.CHISELED_QUARTZ_BLOCK, Material.QUARTZ_BRICKS,
+//			Material.QUARTZ_PILLAR,
+			Material.QUARTZ_BLOCK, Material.SMOOTH_QUARTZ, Material.SNOW_BLOCK };
+	private float max_value, min_value;
+	private boolean inv;
+
+	public ColorScale(float max, float min, boolean inverse) {
+		max_value = max;
+		min_value = min;
+		inv = inverse;
+	}
+
+	public Material getMaterialForValue(float value) {
+		int len = scale.length;
+		if (value >= max_value) {
+			if (inv) {
+				return scale[0];
+			} else {
+				return scale[len];
+			}
+		} else if (value <= min_value) {
+			if (inv) {
+				return scale[len];
+			} else {
+				return scale[0];
+			}
+		} else {
+			if (inv) {
+				return scale[(int) (len - (value + min_value) / (max_value - min_value) * len)];
+			} else {
+				return scale[(int) ((value + min_value) / (max_value - min_value) * len)];
+			}
+		}
+	}
+}
